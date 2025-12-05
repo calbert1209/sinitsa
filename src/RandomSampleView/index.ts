89397f08a26ceae5e18da878c90046cf890072fc
Christian Albert <calbert1209@gmail.com>
2025-12-05 21:25:40 +0900

    use preact in bases view

--- src/RandomSampleView/index.ts
+++ src/RandomSampleView/index.ts
@@ -1,4 +1,5 @@
 import {
+	BasesEntry,
 	BasesView,
 	HoverParent,
 	HoverPopover,
@@ -6,7 +7,10 @@ import {
 } from "obsidian";
 import { SettingsStore } from "src/main";
 import { getViewOptionValue } from "src/ViewOption";
-import { Group } from "./pseudo-components";
+import { createElement, render } from "preact";
+import { App } from "src/App";
+import { dataSignal, Item, itemsSignal } from "src/store";
+import { generateIndices } from "src/randomIndices";
 
 export const RandomSampleViewType = "random-sample-view";
 
@@ -26,24 +30,44 @@ export class RandomSampleView extends BasesView implements HoverParent {
 		this.settingsStore = settingsStore;
 	}
 
+	public onload(): void {
+		console.log("loading", this.data);
+		render(createElement(App, null), this.containerEl);
+	}
+
 	public async onDataUpdated(): Promise<void> {
 		const { app } = this;
-		this.containerEl.empty();
-
 		const { shownProperty, count } = getViewOptionValue(this.config);
-
+		dataSignal.value = `Updated at ${new Date().toISOString()}`;
 		for (const group of this.data.groupedData) {
-			await Group({
-				parent: this.containerEl,
-				app,
-				group,
-				settingsStore: this.settingsStore,
-				shownProperty,
-				count,
-				onDataUpdated: async () => {
-					await this.onDataUpdated();
-				},
-			});
+			const pickedEntries: BasesEntry[] = [];
+			const indices = generateIndices(
+				this.settingsStore.settings.seed,
+				Math.min(count, group.entries.length),
+				group.entries.length
+			);
+			for (const index of indices) {
+				pickedEntries.push(group.entries[index]);
+			}
+
+			const items: Item[] = [];
+
+			for (let i = 0; i < pickedEntries.length; i++) {
+				const entry = pickedEntries[i];
+				const text = (
+					entry.getValue(shownProperty) ?? "..."
+				).toString();
+
+				let score = 0;
+				await app.fileManager.processFrontMatter(
+					entry.file,
+					(fm) => (score = fm.score)
+				);
+
+				items.push({ text, score, file: entry.file });
+			}
+
+			itemsSignal.value = items;
 		}
 	}
 }
